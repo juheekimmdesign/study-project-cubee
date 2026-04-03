fcff55fdbeccf51875b6ea7eb187cb7f5dcea40a
juheekimmdesign <juheekimmdesign@gmail.com>
2026-04-03 18:57:45 +0900

    프로젝트 진행중

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,31 @@
 // index.js
 const topBnr = document.querySelector('.top_bnr_swiper .top_bnr')
+const heroBnr = document.querySelector('.hero_bnr .hero')
+const best = document.querySelector('.best_right .best_swiper')
+console.log(topBnr, heroBnr, best);
+const bestSwiper = new Swiper(best, {
+    slidesPerView:4,
+    spaceBeetween:20,
+})
+
 const topBnrSwiper = new Swiper(topBnr, {
     effect:'fade',
     loop:true,
     autoplay:{delay:5000,},
 })
 console.log(topBnr);
+
+const heroBnrSwiper = new Swiper(heroBnr, {
+    loop:true,
+    // pagination:{
+    //     el:'.hero .page',
+    //     type:'fraction',
+    // },
+    // navigation:{
+    //     prevEl:'.hero .prev',
+    //     nextEl:'.hero .next',
+    // },
+    // scrollbar:{
+    //     el:'.hero .scroll',
+    // },
+})
