b79b9f5d5bf8c169c5d9a9f3a6e04fc88da2e177
juheekimmdesign <juheekimmdesign@gmail.com>
2026-04-06 00:41:40 +0900

    ~8.add_brn.css

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,8 @@ const heroBnr = document.querySelector('.hero_bnr .hero')
 const best = document.querySelector('.best_right .best_swiper')
 const coming = document.querySelector ('.coming_wrap .coming_swiper') 
 const newslide = document.querySelector('.new_wrap .new_swiper')
-console.log( heroBnr, best , coming, newslide);
+const product = document.querySelector('.product_right .product_swiper')
+console.log( heroBnr, best , coming, newslide, product);
 
 
 const bestSwiper = new Swiper(best, {
@@ -32,5 +33,14 @@ const comingSwiper = new Swiper(coming, {
 const newSwiper = new Swiper(newslide, {
     loop: true,
     slidesPerView: 4,
+    spaceBetween: 20,
+})
+const product_swiper = new Swiper(product, {
+    loop: true,
+    slidesPerView: 4,
+    grid: {
+    rows: 2,        // 2줄
+    fill: 'row'     // 가로 → 세로 순서로 채움
+  },
     spaceBetween: 20,
 })
