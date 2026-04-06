349300c04209cd3e79620cb7577f87dbff762e1e
juheekimmdesign <juheekimmdesign@gmail.com>
2026-04-06 13:25:19 +0900

    css 작성중(~event)

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,11 @@
 // index.js
-const heroBnr = document.querySelector('.hero_bnr .hero')
-const best = document.querySelector('.best_right .best_swiper')
-const coming = document.querySelector ('.coming_wrap .coming_swiper') 
-const newslide = document.querySelector('.new_wrap .new_swiper')
-const product = document.querySelector('.product_right .product_swiper')
-console.log( heroBnr, best , coming, newslide, product);
+const heroBnr = document.querySelector('.hero_bnr .hero');
+const best = document.querySelector('.best_right .best_swiper');
+const coming = document.querySelector ('.coming_wrap .coming_swiper');
+const newslide = document.querySelector('.new_wrap .new_swiper');
+const product = document.querySelector('.product_right .product_swiper');
+const mdpick = document.querySelector('.mdpick_right .mdpick_swiper');
+console.log( heroBnr, best , coming, newslide, product, mdpick);
 
 
 const bestSwiper = new Swiper(best, {
@@ -36,11 +37,15 @@ const newSwiper = new Swiper(newslide, {
     spaceBetween: 20,
 })
 const product_swiper = new Swiper(product, {
-    loop: true,
     slidesPerView: 4,
     grid: {
     rows: 2,        // 2줄
-    fill: 'row'     // 가로 → 세로 순서로 채움
-  },
+    fill: 'row',     // 가로 → 세로 순서로 채움
+    },
+    spaceBetween: 20,
+})
+const mdpickSwiper = new Swiper(mdpick, {
+    loop:true,
+    slidesPerView: 4,
     spaceBetween: 20,
 })
