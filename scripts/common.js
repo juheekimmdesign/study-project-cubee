ea7b9a215abeb578a1c8ce582b5a5bf29a7c5eb3
juheekimmdesign <juheekimmdesign@gmail.com>
2026-04-16 16:14:28 +0900

    hover 동적기능 추가중

--- scripts/common.js
+++ scripts/common.js
@@ -1,9 +1,31 @@
 // common.js
-const topBnr = document.querySelector('.top_bnr_swiper .top_bnr')
-console.log(topBnr);
+const topBnr = document.querySelector('.top_bnr_swiper .top_bnr');
+const lnbSelct = document.querySelectorAll('.lnb a');
+console.log(topBnr, lnbSelct[0],lnbSelct[1],lnbSelct[2] );
+
+lnbSelct[0].addEventListener('mouseover', function(){
+    lnbSelct[0].src = './images/common/search_on.svg';
+});
+lnbSelct[0].addEventListener('mouseout', function(){
+    lnbSelct[0].src = './images/common/search.svg';
+});
+
+lnbSelct[1].addEventListener('mouseover', function(){
+    lnbSelct[1].src = './images/common/cart_on.svg';
+});
+lnbSelct[1].addEventListener('mouseout', function(){
+    lnbSelct[1].src = './images/common/cart.svg';
+});
+
+lnbSelct[2].addEventListener('mouseover', function(){
+    lnbSelct[2].src = './images/common/user_on.svg';
+});
+lnbSelct[2].addEventListener('mouseout', function(){
+    lnbSelct[2].src = './images/common/user.svg';
+});
 
 const topBnrSwiper = new Swiper(topBnr, {
     effect:'fade',
     loop:true,
     autoplay:{delay:2000,},
-})
+});
--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,29 @@ const best = document.querySelector('.best_right .best_swiper');
 const coming = document.querySelector ('.coming_wrap .coming_swiper');
 const newslide = document.querySelector('.new_wrap .new_swiper');
 const mdpick = document.querySelector('.mdpick_right .mdpick_swiper');
-console.log( heroBnr, best , coming, newslide, mdpick);
+const artistSelect = document.querySelectorAll('.artist_right a img')
+console.log( heroBnr, best , coming, newslide, mdpick, artistSelect[0],artistSelect[1],artistSelect[2]);
+
+artistSelect[0].addEventListener('mouseover',function(){
+    artistSelect[0].src = './images/product/artist_04.jpg';
+});
+artistSelect[0].addEventListener('mouseout',function(){
+    artistSelect[0].src = './images/product/artist_01.jpg';
+});
+
+artistSelect[1].addEventListener('mouseover',function(){
+    artistSelect[1].src = './images/product/artist_05.jpg';
+});
+artistSelect[1].addEventListener('mouseout',function(){
+    artistSelect[1].src = './images/product/artist_02.jpg';
+});
+
+artistSelect[2].addEventListener('mouseover',function(){
+    artistSelect[2].src = './images/product/artist_06.jpg';
+});
+artistSelect[2].addEventListener('mouseout',function(){
+    artistSelect[2].src = './images/product/artist_03.jpg';
+});
 
 
 const bestSwiper = new Swiper(best, {
